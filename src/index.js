e3fde4abad82bbf6e19713da1afe3722ae7cc03b
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2023-01-10 11:36:50 +0200

    fix deploy problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
